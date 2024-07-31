2db0b1f547682eb6acf6a7434978d381d3e22185
Rayan Silva <rayan.lima@shopdelivery.com.br>
2024-07-31 12:10:53 -0300

    pwa2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'quiz-app';
+  private deferredPrompt: any;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(event: any) {
+    // Evita o prompt automático
+    event.preventDefault();
+    this.deferredPrompt = event;
+    // Exibe um botão ou algum elemento para o usuário interagir
+    this.showInstallButton = true;
+  }
+
+  showInstallButton: boolean = false;
+
+  installApp() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('Usuário aceitou o prompt de instalação');
+        } else {
+          console.log('Usuário rejeitou o prompt de instalação');
+        }
+        this.deferredPrompt = null;
+        this.showInstallButton = false;
+      });
+    }
+  }
 }
